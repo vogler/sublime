3ca2b9cb64ed1954274ce6d98dbdb08176fc2f7f
vogler <ralf.vogler@gmail.com>
2013-03-12 13:31:51 +0100

    support for ocamlbuild (scans for _build directory)

--- OCaml/ocaml_annot.py
+++ OCaml/ocaml_annot.py
@@ -5,21 +5,32 @@ import re
 class OcamlAnnotCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		path_ml   = path.abspath(self.view.file_name())
-		basename  = path.basename(path_ml)
-		base, ext = path.splitext(basename)
+		dir_ml    = path.dirname(path_ml)
+		filename  = path.basename(path_ml)
+		base, ext = path.splitext(filename)
 		if ext!='.ml':
 			sublime.status_message('OCaml annotations only work for .ml-files')
 			return
-		path_annot = path.join(path.dirname(path_ml), base+'.annot')
-		if not path.exists(path_annot):
-			sublime.status_message('.annot-file not found! Run ocamlopt -annot '+basename)
-			return
+		filename_annot = base+'.annot'
+		path_annot = path.join(dir_ml, filename_annot)
+		if not path.exists(path_annot): # .annot-file is not in the same directory as the source-file
+			# ocamlbuild: go up the path and check if there is a directory called _build somewhere
+			cwd = dir_ml; cwd_old = ''
+			while cwd != cwd_old: # once at root, the path doesn't change anymore
+				dir_src    = dir_ml[len(path.commonprefix([dir_ml, cwd]))+1:]
+				path_annot  = path.join(path.join(path.join(cwd, '_build'), dir_src), filename_annot)
+				# print(cwd, dir_src, path_annot)
+				if path.exists(path_annot): break
+				cwd_old = cwd
+				cwd = path.abspath(path.join(cwd, path.pardir)) # go up one directory
+			if not path.exists(path_annot): # nothing was found
+				sublime.status_message('.annot-file not found! Run ocamlopt -annot '+filename)
+				return
 		if len(self.view.sel()) > 1:
 			sublime.status_message('More than one region is selected. Will only take the first one.')
 		region = self.view.sel()[0]
 		if region.empty(): # grow selection one char to to the right if it is empty
 			region = sublime.Region(region.begin(), region.end()+1)
-		# print self.view.lines(region)
 
 		# maybe use https://github.com/avsm/ocaml-annot
 		f = open(path_annot)
